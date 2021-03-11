17dff6d6e4773246d3512818f7e0c05ec50f642c
I <novenkaya1907@mail.ru>
2021-03-11 17:34:27 +0300

    add new components

---
+++ src/Components/Left part/Left.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Left.module.css';
+
+const Left = () => {
+    return (
+        <div dir='rtl' className={style.content}>
+            
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default Left
---
+++ src/Components/Right part/Right.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Right.module.css';
+import emblem from '../../img/Emblem.png'
+
+const Right = () => {
+    return (
+        <div dir='rtl' className={style.content}>
+            <div className={style.infoLabel}>
+                <div><img src={emblem} alt="embleme"></img></div>
+                <div className={style.infoText}>
+                    <div className={style.boldText}>לארשי תרטשמ</div>
+                    <div className={style.text}>רלוג תילא גניסיפידא ררוטקסנוק ,טמא טיס <br/> רולוד םוספיא םרול</div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Right
